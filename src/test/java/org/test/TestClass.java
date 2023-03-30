c589db026936d70c4727ad7e40c3e662dd27a0d0
dineshkumardk26 <mysystem978@gmail.com>
2023-03-30 16:33:02 +0530

    Dinesh's code

--- src/test/java/org/test/TestClass.java
+++ src/test/java/org/test/TestClass.java
@@ -1,9 +1,11 @@
 package org.test;
 
-public class Test {
-
-	public Test() {
-		// TODO Auto-generated constructor stub
+public class TestClass {
+		
+		private void test() {
+			
+			
+			
 	}
 
 }
